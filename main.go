013c67a600a93643a974fb953ee8577176880324
WolfDan <5377526+WolfDan@users.noreply.github.com>
2025-11-03 23:09:50 -0500

    expose provider to public

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"flag"
 	"log"
 
-	"github.com/supabase/terraform-provider-supabase-analytics/internal/provider"
+	"github.com/supabase/terraform-provider-supabase-analytics/provider"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
